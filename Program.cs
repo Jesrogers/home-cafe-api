44d2a9e417b3516d39bb8ee9b34c1027eaedabc7
Jesse Rogers <jesserileyrogers@gmail.com>
2025-12-07 13:22:50 -0500

    added CORS policy

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using HomeCafeApi.Database;
 using HomeCafeApi.Endpoints;
 using Microsoft.EntityFrameworkCore;
 
+var MyAllowedOrigins = "_myAllowedOrigins";
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<HomeCafeDb>(opt => opt.UseInMemoryDatabase("HomeCafeDb"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
@@ -10,7 +11,19 @@ builder.Services.ConfigureHttpJsonOptions(options =>
     options.SerializerOptions.WriteIndented = true;
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowedOrigins,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
+app.UseCors(MyAllowedOrigins);
 app.RegisterMenuItemsEndpoints();
 app.Run();
